9f793046f3ea4fb3aabc4a47e106165104304b61
OumarBah506 <bahoumar798@gmail.com>
2026-04-05 22:13:10 -0300

    Update Queries.sql

--- Scripts/Queries.sql
+++ Scripts/Queries.sql
@@ -5,3 +5,12 @@
 -- ==============================================================================
 -- 1. LENDER EFFICIENCY
 -- Show the average processing time for each lender and the 14-day gap between Fairstone and the other lenders.
+SELECT 
+    Lender,
+    ROUND(AVG(DATEDIFF(Date_Funded, Date_Received)),0) AS Avg_Processing_Days
+FROM fct_data
+GROUP BY Lender
+ORDER BY Avg_Processing_Days DESC;
+
+
+--2. 
